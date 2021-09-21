5341b9b9fa5d71233f8b557bb0a392c949c8a0dc
Beshoy Samy <beshoy.samy411@gmail.com>
2021-09-21 03:13:55 +0200

    finished GetPaymentMethodsUseCase and its implementation

--- payment/payment-domain/src/main/java/com/paynoneer/payment/domain/usecases/GetPaymentMethodsUseCase.java
+++ payment/payment-domain/src/main/java/com/paynoneer/payment/domain/usecases/GetPaymentMethodsUseCase.java
@@ -1,4 +1,12 @@
 package com.paynoneer.payment.domain.usecases;
 
+import com.paynoneer.payment.domain.entities.PaymentMethodDomainModel;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.core.Single;
+
 public interface GetPaymentMethodsUseCase {
+
+    Single<List<PaymentMethodDomainModel>> getPaymentMethods();
 }
